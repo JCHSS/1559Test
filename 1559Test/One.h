3b7e924f75691dbd90f5db710c98d320cd58750b
JCHSS <136691850@qq.com>
2015-12-28 14:17:59 +0800

    a

---
+++ 1559Test/One.h
@@ -0,0 +1,13 @@
+//
+//  One.h
+//  1559Test
+//
+//  Created by MS on 15-12-28.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface One : UITabBarController
+
+@end
---
+++ 1559Test/One.m
@@ -0,0 +1,13 @@
+//
+//  One.m
+//  1559Test
+//
+//  Created by MS on 15-12-28.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import "One.h"
+
+@implementation One
+
+@end
